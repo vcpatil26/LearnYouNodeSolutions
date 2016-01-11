a7bbf5d5bfb68d79bdae04ec40b45805e459d628
vcpatil <vcpatil@indiana.edu>
2016-01-11 16:59:15 -0600

    All challenges completed

---
+++ httpUC/program.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var map = require('through2-map');
+
+uc = map(function(chunk) {
+  return chunk.toString().toUpperCase();
+});
+
+server = http.createServer(function(request, response) {
+  if (request.method == 'POST') {
+    request.pipe(uc).pipe(response);
+  }
+});
+server.listen(process.argv[2]);
